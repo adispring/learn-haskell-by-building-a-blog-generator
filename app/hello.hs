f1539bef8b76b08b2d3c4a4301a805f552451344
adispring <sunnyadi@163.com>
2022-12-17 22:16:06 +0800

    feat: tag by el

--- app/hello.hs
+++ app/hello.hs
@@ -4,12 +4,6 @@ main = putStrLn myHtml
 myHtml :: String
 myHtml = makeHtml "My page title" "My page content"
 
-head_ :: String -> String
-head_ content = "<head>" <> content <> "</head>"
-
-title_ :: String -> String
-title_ content = "<title>" <> content <> "</title>"
-
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
@@ -22,3 +16,9 @@ html_ = el "html"
 
 body_ :: String -> String
 body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
