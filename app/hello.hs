8f9119409cd16d461c944670ca0381aa21ce00f0
adispring <sunnyadi@163.com>
2022-12-17 22:14:12 +0800

    feat: 添加类型签名

--- app/hello.hs
+++ app/hello.hs
@@ -1,11 +1,16 @@
+main :: IO ()
 main = putStrLn myHtml
 
+myHtml :: String
 myHtml = makeHtml "My page title" "My page content"
 
+head_ :: String -> String
 head_ content = "<head>" <> content <> "</head>"
 
+title_ :: String -> String
 title_ content = "<title>" <> content <> "</title>"
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 el :: String -> String -> String
