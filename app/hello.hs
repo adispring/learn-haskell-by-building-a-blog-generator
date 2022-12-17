cd2eb833c94553a14e162a3ac55680dc9f66b3a1
adispring <sunnyadi@163.com>
2022-12-17 21:46:16 +0800

    feat: 3.2 Adding type signatures

--- app/hello.hs
+++ app/hello.hs
@@ -2,13 +2,18 @@ main = putStrLn myHtml
 
 myHtml = makeHtml "My page title" "My page content"
 
-html_ content = "<html>" <> content <> "</html>"
-
-body_ content = "<body>" <> content <> "</body>"
-
 head_ content = "<head>" <> content <> "</head>"
 
 title_ content = "<title>" <> content <> "</title>"
 
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
