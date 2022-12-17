7982b98097c122e15106e157b271ad75657cbb49
adispring <sunnyadi@163.com>
2022-12-17 22:24:44 +0800

    feat: richer html2

--- app/hello.hs
+++ app/hello.hs
@@ -2,10 +2,13 @@ main :: IO ()
 main = putStrLn myHtml
 
 myHtml :: String
-myHtml = makeHtml "My page title" "My page content"
+myHtml = 
+  makeHtml
+    "Hello title"
+    (h1_ "Hello, world" <> p_ "Let's learn about Haskell!")
 
 makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ (h1_ "Hello" <> p_ "world!"))
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 el :: String -> String -> String
 el tag content =
