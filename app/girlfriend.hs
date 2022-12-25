b165452e0d6c32af7e62a328fa9507b906eae969
adispring <sunnyadi@163.com>
2022-12-25 11:55:00 +0800

    feat: withFile

---
+++ app/girlfriend.hs
@@ -0,0 +1,16 @@
+import System.IO
+
+main = do
+  withFile'
+    "girlfriend.txt"
+    ReadMode
+    (\handle -> do
+      contents <- hGetContents handle
+      putStr contents)
+
+withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile' path mode f = do
+  handle <- openFile path mode
+  result <- f handle
+  hClose handle
+  return result
